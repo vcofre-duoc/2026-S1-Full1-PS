591b24323e50dc4e91f84092cada01cb792a991c
Victor Cofre F <victor.cofre.f@gmail.com>
2026-06-01 17:02:28 -0400

    ajustes menores de configuracion

--- democars/src/main/java/cl/duoc/democars/config/SecurityConfig.java
+++ democars/src/main/java/cl/duoc/democars/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/v1/cars/list2").permitAll()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(new TokenValidationFilter(authService),
